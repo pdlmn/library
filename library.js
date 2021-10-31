563fc1ddf2789fc5e3618b2c459c93ee46d8a24d
pdlmn <djeinwings@gmail.com>
2021-10-31 03:10:20 +0300

    Added localStorage stuff and made everything prettier

--- library.js
+++ library.js
@@ -3,73 +3,112 @@ const submit = document.querySelector('input[type=submit]');
 const toggleFormButton = document.querySelector('#toggle-form')
 const hiddenForm = document.querySelector('#hidden-form');
 
-let myLibrary = [{id: 1, title: 'War and peace', author: 'Leon Tolstoy', pages: 1000, read: true}];
-let newBook = new Book('Lolita', 'Nabokov', 500, false);
-addBookToLibrary(newBook);
+let myLibrary = [];
+
+parseLocalStorage();
+displayBookCards();
 
 function Book(title, author, pages, read) {
-  this.id = myLibrary[myLibrary.length - 1].id + 1;
+  myLibrary.at(-1) ? this.id = myLibrary.at(-1).id + 1 : this.id = 1;
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary(book) {
+function addBookToLibrary(e) {
+  e.preventDefault();
+
+  titleInput = document.querySelector('#title');
+  authorInput = document.querySelector('#author');
+  pagesInput = document.querySelector('#pages');
+  readInput = document.querySelector('#read');
+
+  let book = new Book(titleInput.value, authorInput.value, pagesInput.value, readInput.checked);
+
+  titleInput.value = '';
+  authorInput.value = '';
+  pagesInput.value = '';
+  readInput.checked = false;
+
   myLibrary.push(book);
+  createBookCard(book);
+
+  saveLibraryToLocalStorage();
 }
 
-function displayBooks() {
+function displayBookCards() {
   for (book of myLibrary) {
-    bookWrapper = document.createElement('div');
-    bookWrapper.classList.add('book');
-    bookWrapper.dataset.id = book.id;
-    bookWrapper.innerHTML = 
-      `<div>${book.title}</div>
-       <div>${book.author}</div>
-       <div>${book.pages}</div>
-       <div>${book.read}</div>`; 
-    readBookButton = document.createElement('button');
-    deleteBookButton = document.createElement('button');
-    readBookButton.textContent = 'Read';
-    deleteBookButton.textContent = 'Delete';
-    book.read ? readBookButton.classList.add('is-read') : readBookButton.classList.add('not-read'); 
-    readBookButton.addEventListener('click', readBook);
-    deleteBookButton.addEventListener('click', deleteBook);
-    bookWrapper.appendChild(readBookButton);
-    bookWrapper.appendChild(deleteBookButton);
-    books.appendChild(bookWrapper);
+    createBookCard(book);
   }
 }
 
-function readBook(e) {
-  bookWrapper = e.target.parentNode;
-  bookId = bookWrapper.dataset.id;
+function createBookCard(book) {
+  let bookWrapper = document.createElement('div');
+  bookWrapper.classList.add('book');
+  bookWrapper.dataset.id = book.id;
+  bookWrapper.innerHTML = 
+    `<div>"${book.title}"</div>
+    <div>${book.author}</div>
+    <div>${book.pages} pages</div>`; 
 
-  bookIndex = myLibrary.findIndex(book => book.id === Number(bookId));
-  myLibrary[bookIndex].read = !myLibrary[bookIndex].read;
+  let readBookButton = document.createElement('button');
+  readBookButton.classList.add('button');
+  book.read ? readBookButton.classList.add('is-read') : readBookButton.classList.add('not-read'); 
+  book.read ? readBookButton.textContent = 'Read' : readBookButton.textContent = 'Not Read';
+
+  let deleteBookButton = document.createElement('button');
+  deleteBookButton.classList.add('button');
+  deleteBookButton.classList.add('delete');
+  deleteBookButton.textContent = 'Delete';
+
+  readBookButton.addEventListener('click', toggleRead);
+  deleteBookButton.addEventListener('click', deleteBook);
+
+  bookWrapper.appendChild(readBookButton);
+  bookWrapper.appendChild(deleteBookButton);
+  books.appendChild(bookWrapper);
+}
+
+function toggleRead(e) {
+  let bookWrapper = e.target.parentNode;
+  let bookId = bookWrapper.dataset.id;
+
+  let bookIndex = myLibrary.findIndex(book => book.id === Number(bookId));
+  let book = myLibrary[bookIndex];
+  book.read = !book.read;
+
   e.target.classList.toggle('is-read');
   e.target.classList.toggle('not-read');
-  console.log(myLibrary[bookIndex].read);
+
+  book.read ? e.target.textContent = 'Read' : e.target.textContent = 'Not Read';
+
+  saveLibraryToLocalStorage();
 }
 
 function deleteBook(e) {
-  bookWrapper = e.target.parentNode;
-  bookId = bookWrapper.dataset.id;
+  let bookWrapper = e.target.parentNode;
+  let bookId = bookWrapper.dataset.id;
 
   myLibrary = myLibrary.filter(book => book.id !== Number(bookId));
   bookWrapper.remove();
+
+  saveLibraryToLocalStorage();
 }
 
 function showForm() {
   hiddenForm.classList.toggle('invisible');
 }
 
-function getBook(e) {
-  e.preventDefault();
+function parseLocalStorage() {
+  let stringifiedLibrary = localStorage.getItem('myLibrary');
+  myLibrary = JSON.parse(stringifiedLibrary);
 }
 
-displayBooks();
+function saveLibraryToLocalStorage() {
+  let stringifiedLibrary = JSON.stringify(myLibrary);
+  localStorage.setItem('myLibrary', stringifiedLibrary);
+}
 
 toggleFormButton.addEventListener('click', showForm);
-submit.addEventListener('click', getBook);
+submit.addEventListener('click', addBookToLibrary);
